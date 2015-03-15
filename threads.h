7ffa5843a6811fc7ddb6499b3b27f9ebc184649a
Joshua Stark <JMRante@users.noreply.github.com>
2015-03-14 21:36:43 -0700

    Update threads.h
    Added what needed to be added. Exciting.

--- threads.h
+++ threads.h
@@ -1,16 +1,26 @@
 #include "q.h"
+#include <stdlib.h>
+
+TCB_t* RunQ;
 
 void start_thread(void (*function)())
 {
-	
+    stack_t* stack = (stack_t*)malloc(8192);
+    TCB_t* thread = NewItem();
+    init_TCB(thread, function, stack, 8192);
+    AddQueue(&RunQ, thread);
 }
 
 void run()
 {
-	
+    ucontext_t parent;
+    getcontext(&parent);
+    swapcontext(&parent, &(RunQ->context));
 }
 
 void yield()
 {
-	
+    TCB_t* previousThread = RunQ;
+    RotateQ(&RunQ);
+    swapcontext(&(previousThread->context), &(RunQ->context)); 
 }
