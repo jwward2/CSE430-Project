0fa61f45ae9319ae6556c50106dcbe260718b2c2
Joshua Stark <JMRante@users.noreply.github.com>
2015-03-19 09:28:43 -0700

    Added Two New Functions
    Clock tells time and Fibonacci does himself.

--- thread_test.c
+++ thread_test.c
@@ -7,22 +7,33 @@
 #include "threads.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <time.h>
 
-void function_1()
+int fibonacciA;
+int fibonacciB;
+
+//This function shows the current time since the process began
+void function_clock()
 {
     while(1)
     {
-        printf("Hello I'm function 1!\n");
+	time_t t;
+	time(&t);
+        printf("The current time is %s", ctime(&t));
         sleep(1);
         yield();
     }
 }
 
-void function_2()
+//Uses its own local variables and a global variable to generate the Fibonacci sequence over time
+void function_fibonacci()
 {
     while(1)
     {
-        printf("Hello I'm function 2!\n");
+        int localResult = fibonacciA + fibonacciB;
+	fibonacciB = fibonacciA;
+	fibonacciA = localResult;
+        printf("Fibonacci mumbles aloud %d.\n", localResult);
         sleep(1);
         yield();
     }
@@ -50,9 +61,12 @@ void function_4()
 
 int main()
 {
+    fibonacciA = 1;
+    fibonacciB = 0;
+  
     InitQueue(&RunQ);
-    start_thread(function_1);
-    start_thread(function_2);
+    start_thread(function_clock);
+    start_thread(function_fibonacci);
     start_thread(function_3);
     start_thread(function_4);
 	
